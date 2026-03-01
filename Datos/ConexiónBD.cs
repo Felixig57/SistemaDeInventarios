9d3b2ba014aa6326698ce8379449a39d08bac3d7
TenPato <aaronnga1210@gmail.com>
2026-02-28 17:47:10 -0700

    Creé la clase Conexion BD con los metodos AbrirConexion para abrir la conexion con la base de datos y el metodo CerrarConexión para cerrarla,

---
+++ Datos/ConexiónBD.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Datos
+{
+    public class ConexiónBD
+    {
+
+        protected SqlConnection conexion;
+
+        public ConexiónBD()
+        {
+            conexion = new SqlConnection("Server=AARONCOMPU\\AARONNDEVGG2;Database=SistemaInventarios;Integrated Security=True;");
+        }
+
+        public void AbrirConexion()
+        {
+            if (conexion.State == System.Data.ConnectionState.Closed)
+                conexion.Open();
+            MessageBox.Show("Conexión abierta exitosamente.");
+        }
+
+        public void CerrarConexion()
+        {
+            if (conexion.State == System.Data.ConnectionState.Open)
+                conexion.Close();
+            MessageBox.Show("Conexión cerrada exitosamente.");
+        }
+    }
+}
