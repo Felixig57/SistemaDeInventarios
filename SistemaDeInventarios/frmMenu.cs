e2022812a0e7797457a72c0b8f44eccb0a8cf81f
Felixig57 <146308455+Felixig57@users.noreply.github.com>
2026-02-28 18:43:15 -0700

    Cambios en el diseno para finalizar

--- SistemaDeInventarios/frmMenu.cs
+++ SistemaDeInventarios/frmMenu.cs
@@ -32,5 +32,18 @@ namespace SistemaDeInventarios
             frmProductos frmProductos = new frmProductos();
             frmProductos.ShowDialog();
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            frmProveedores frmProveedores = new frmProveedores();
+            frmProveedores.ShowDialog();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            frmAlmacenes frmAlmacenes   = new frmAlmacenes();
+            frmAlmacenes.ShowDialog();
+        }
     }
 }
--- SistemaDeInventarios/frmProductos.Designer.cs
+++ SistemaDeInventarios/frmProductos.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.button6 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
@@ -99,14 +99,14 @@
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.GradientActiveCaption;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Id,
@@ -160,12 +160,13 @@
             // 
             // button4
             // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(133)))), ((int)(((byte)(186)))));
             this.button4.Location = new System.Drawing.Point(24, 175);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(150, 45);
             this.button4.TabIndex = 3;
             this.button4.Text = "Limpiar";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.UseVisualStyleBackColor = false;
             // 
             // label3
             // 
@@ -179,21 +180,23 @@
             // 
             // button3
             // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(133)))), ((int)(((byte)(186)))));
             this.button3.Location = new System.Drawing.Point(24, 124);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(150, 45);
             this.button3.TabIndex = 2;
             this.button3.Text = "Eliminar";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(133)))), ((int)(((byte)(186)))));
             this.button1.Location = new System.Drawing.Point(24, 24);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(150, 45);
             this.button1.TabIndex = 0;
             this.button1.Text = "Agregar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // groupBox3
             // 
@@ -207,16 +210,16 @@
             this.groupBox3.Size = new System.Drawing.Size(196, 234);
             this.groupBox3.TabIndex = 3;
             this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "groupBox3";
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(133)))), ((int)(((byte)(186)))));
             this.button2.Location = new System.Drawing.Point(24, 75);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(150, 45);
             this.button2.TabIndex = 1;
             this.button2.Text = "Editar";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // txtDescripcionProducto
             // 
@@ -265,7 +268,6 @@
             this.groupBox2.Size = new System.Drawing.Size(1104, 305);
             this.groupBox2.TabIndex = 5;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "groupBox2";
             // 
             // groupBox1
             // 
@@ -292,7 +294,6 @@
             this.groupBox1.Size = new System.Drawing.Size(1104, 253);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
             // 
             // groupBox4
             // 
@@ -307,7 +308,6 @@
             this.groupBox4.Size = new System.Drawing.Size(430, 234);
             this.groupBox4.TabIndex = 13;
             this.groupBox4.TabStop = false;
-            this.groupBox4.Text = "groupBox4";
             // 
             // button5
             // 
@@ -411,7 +411,7 @@
             // 
             this.gbTituloC.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.gbTituloC.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.gbTituloC.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(133)))), ((int)(((byte)(186)))));
             this.gbTituloC.Controls.Add(this.button6);
             this.gbTituloC.Controls.Add(this.label3);
             this.gbTituloC.Location = new System.Drawing.Point(12, 12);
@@ -419,12 +419,12 @@
             this.gbTituloC.Size = new System.Drawing.Size(1113, 100);
             this.gbTituloC.TabIndex = 3;
             this.gbTituloC.TabStop = false;
-            this.gbTituloC.Text = "groupBox1";
             // 
             // frmProductos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(239)))), ((int)(((byte)(242)))));
             this.ClientSize = new System.Drawing.Size(1125, 716);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
